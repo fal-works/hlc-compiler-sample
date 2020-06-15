4818a4b2476dc956acdd623f924bbbc52215d364
FAL <falworks.contact@gmail.com>
2020-06-16 03:40:17 +0900

    code format

--- Main.hx
+++ Main.hx
@@ -3,31 +3,31 @@ class Main extends hxd.App {
 		new Main();
 
 	override function init() {
-    playSound();
-    optionals();
-  }
+		playSound();
+		optionals();
+	}
 
-  function playSound() {
-    hxd.Res.initLocal();
-    hxd.Res.sound.play();
+	function playSound() {
+		hxd.Res.initLocal();
+		hxd.Res.sound.play();
 
-    hxd.Window.getInstance().onClose = () -> {
-      hxd.snd.Manager.get().dispose();
-      return true;
-    };
-  }
+		hxd.Window.getInstance().onClose = () -> {
+			hxd.snd.Manager.get().dispose();
+			return true;
+		};
+	}
 
-  function optionals() {
-    #if TEST_UV
-    new hl.uv.Tcp();
-    #end
+	function optionals() {
+		#if TEST_UV
+		new hl.uv.Tcp();
+		#end
 
-    #if TEST_SSL
-    new sys.ssl.Socket();
-    #end
+		#if TEST_SSL
+		new sys.ssl.Socket();
+		#end
 
-    #if TEST_MYSQL
-    final connectMysql = sys.db.Mysql.connect;
-    #end
-  }
+		#if TEST_MYSQL
+		final connectMysql = sys.db.Mysql.connect;
+		#end
+	}
 }
